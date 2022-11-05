94bfe41281a1ed2eef91cd4013b91aa3404e100f
a2tikhonov <a2tikhonov@yandex.ru>
2022-11-05 18:12:13 +0300

    Инициализировал пустой spring-boot проект

---
+++ src/test/java/ru/skypro/jd6/team3/animalshelter/AnimalShelterApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.skypro.jd6.team3.animalshelter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnimalShelterApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
