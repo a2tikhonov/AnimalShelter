f7abec6ab60442cb8c069141f9627f0a416ef95c
Anton <imewokrus@gmail.com>
2022-11-16 13:03:18 +0300

    Обновил контроллеры для OwnerService и PetService

--- src/main/java/ru/skypro/jd6/team3/animalshelter/controller/PetController.java
+++ src/main/java/ru/skypro/jd6/team3/animalshelter/controller/PetController.java
@@ -1,4 +1,65 @@
 package ru.skypro.jd6.team3.animalshelter.controller;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import ru.skypro.jd6.team3.animalshelter.entity.Pet;
+import ru.skypro.jd6.team3.animalshelter.record.PetRecord;
+import ru.skypro.jd6.team3.animalshelter.service.PetService;
+
+@RestController
+@RequestMapping("/pet")
 public class PetController {
+
+    private final PetService petService;
+
+    public PetController(PetService petService) {
+        this.petService = petService;
+    }
+
+    /**
+     * Создаёт Питомец в базе данных
+     * @param pet Передаваемый упрощённый Питомец
+     * @return Возвращает полноценную сущность Питомец из базы данных
+     */
+    @PostMapping
+    public ResponseEntity<PetRecord> createPet(@RequestBody PetRecord pet) {
+        return ResponseEntity.status(HttpStatus.CREATED)
+                .body(petService.createPet(pet));
+    }
+
+    /**
+     * Возвращает Питомец из базы данных
+     * @param id Параметр по которому мы ищём Питомец в базе данных
+     * @return Возвращает Питомец из базы данных если найден, если нет то сообщает об ошибке
+     */
+    @GetMapping("{id}")
+    public ResponseEntity<Pet> getPet(@PathVariable Long id) {
+        Pet pet = petService.getPet(id);
+        if (pet == null) {
+            return ResponseEntity.notFound().build();
+        }
+        return ResponseEntity.ok().build();
+    }
+
+    /**
+     * Обновляет Питомец в базе данных
+     * @param petRecord Данные для обновления в базе данных
+     * @return Возвращает обновлённую сущность из базы данных
+     */
+    @PutMapping
+    public PetRecord updatePet(@RequestBody PetRecord petRecord) {
+        return petService.updatePet(petRecord);
+    }
+
+    /**
+     * Удаляет Питомец из базы данных
+     * @param id Параметр по которому ищется Хозяин в базе данных
+     * @return Возвращает успшность данной операции
+     */
+    @DeleteMapping("{id}")
+    public ResponseEntity<?> deletePet(@PathVariable Long id) {
+        petService.deletePet(id);
+        return ResponseEntity.ok().build();
+    }
 }
