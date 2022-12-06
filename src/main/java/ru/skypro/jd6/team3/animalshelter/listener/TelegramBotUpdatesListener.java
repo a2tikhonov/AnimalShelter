f3990e0151095e92750feee82b59876a635fe5ea
atikhonov <a2tikhonov@yandex.ru>
2022-12-06 17:52:38 +0300

    Реализовал возможность соединить пользователя с волонтером.

--- src/main/java/ru/skypro/jd6/team3/animalshelter/listener/TelegramBotUpdatesListener.java
+++ src/main/java/ru/skypro/jd6/team3/animalshelter/listener/TelegramBotUpdatesListener.java
@@ -4,9 +4,6 @@ import com.pengrad.telegrambot.TelegramBot;
 import com.pengrad.telegrambot.UpdatesListener;
 import com.pengrad.telegrambot.model.Update;
 import com.pengrad.telegrambot.request.SendMessage;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 import ru.skypro.jd6.team3.animalshelter.entity.PotentialOwner;
 import ru.skypro.jd6.team3.animalshelter.entity.Volunteer;
@@ -17,12 +14,9 @@ import ru.skypro.jd6.team3.animalshelter.service.VolunteerService;
 
 import javax.annotation.PostConstruct;
 import java.util.List;
-import java.util.regex.Pattern;
 
 @Service
 public class TelegramBotUpdatesListener implements UpdatesListener {
-
-    private final Logger logger = LoggerFactory.getLogger(TelegramBotUpdatesListener.class);
 
     private final TelegramBot telegramBot;
 
@@ -51,8 +45,6 @@ public class TelegramBotUpdatesListener implements UpdatesListener {
         this.potentialOwnerService = potentialOwnerService;
         this.volunteerService = volunteerService;
     }
-
-    private static final Pattern pattern = Pattern.compile("([0-9.:\\s]{16})(\\s)([\\W+|\\w]+)");
 
     @PostConstruct
     public void init() {
